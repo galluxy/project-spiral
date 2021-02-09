09936c532db7d372929a4d4460739b7005a2bb7c
denimcouch <alexmata92@ymail.com>
2021-02-09 15:51:12 -0600

    NPC class created

--- the_app/main.py
+++ the_app/main.py
@@ -4,7 +4,6 @@ import random_npc as rn
 def main():
     race = rn.random_race()
     name = rn.random_name(race)
-    new_npc = NPC(name, race)
     print(f'Your NPC is a', race, 'named', name)
 
 
